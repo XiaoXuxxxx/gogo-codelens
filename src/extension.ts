b43a63db8ceee4d1c87fe9eb4d6bdeab6d23e7d6
xiaoXuxxxx <aonrok555@gmail.com>
2025-12-18 22:02:14 +0700

    refactor: extract the register logic to GogoCodeLensExtension class

--- src/extension.ts
+++ src/extension.ts
@@ -18,184 +18,182 @@ import { TypeAliasSymbolHandler } from '@/src/symbol/handler/TypeAliasSymbolHand
 import { VariableSymbolHandler } from '@/src/symbol/handler/VariableSymbolHandler';
 import { VsCodeWrapper } from '@/src/vscode/VsCodeWrapper';
 
-function createCodeLensProvider(
-  configLoader: VsCodeGoLensConfigurationLoader,
-  vsCodeWrapper: VsCodeWrapper,
-): RelationCodeLensProvider {
-  const config = configLoader.getConfiguration();
+class GogoCodeLensExtension implements vscode.Disposable {
+  private readonly vsCodeWrapper: VsCodeWrapper;
+  private readonly configLoader: VsCodeGoLensConfigurationLoader;
 
-  const functionReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.functionSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.functionSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.functionSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.functionSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+  private providerDisposable: vscode.Disposable | undefined;
 
-  // interface
-  const interfaceReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.interfaceSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.interfaceSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.interfaceSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.interfaceSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+  public constructor(vsCodeWrapper: VsCodeWrapper, configLoader: VsCodeGoLensConfigurationLoader) {
+    this.vsCodeWrapper = vsCodeWrapper;
+    this.configLoader = configLoader;
+  }
 
-  const interfaceImplementByCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.interfaceSymbolEntryConfig.implementByDisplayRule.singularTemplate),
-    new TemplateRenderer(config.interfaceSymbolEntryConfig.implementByDisplayRule.pluralTemplate),
-    config.interfaceSymbolEntryConfig.implementByDisplayRule.emptyText,
-    config.interfaceSymbolEntryConfig.implementByDisplayRule.shouldShow && config.shouldShowImplementation,
-  );
+  public register(): void {
+    this.providerDisposable?.dispose();
 
-  // child method of interface
-  const childMethodInterfaceReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+    this.configLoader.updateConfiguration();
+    const provider = this.createCodeLensProvider();
 
-  const childMethodInterfaceImplementByCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.singularTemplate),
-    new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.pluralTemplate),
-    config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.emptyText,
-    config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.shouldShow && config.shouldShowImplementation,
-  );
+    this.providerDisposable = vscode.languages.registerCodeLensProvider({ scheme: 'file', language: 'go' }, provider);
+  }
 
-  // method
-  const methodReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.methodSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.methodSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.methodSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.methodSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+  public dispose(): void {
+    this.providerDisposable?.dispose();
+  }
 
-  const methodImplementFromCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.methodSymbolEntryConfig.implementFromDisplayRule.singularTemplate),
-    new TemplateRenderer(config.methodSymbolEntryConfig.implementFromDisplayRule.pluralTemplate),
-    config.methodSymbolEntryConfig.implementFromDisplayRule.emptyText,
-    config.methodSymbolEntryConfig.implementFromDisplayRule.shouldShow && config.shouldShowImplementation,
-  );
+  private createCodeLensProvider(): RelationCodeLensProvider {
+    const config = this.configLoader.getConfiguration();
 
-  // struct
-  const structReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.structSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.structSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.structSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.structSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+    const functionReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.functionSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.functionSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.functionSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.functionSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
 
-  const structImplementFromCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.structSymbolEntryConfig.implementFromDisplayRule.singularTemplate),
-    new TemplateRenderer(config.structSymbolEntryConfig.implementFromDisplayRule.pluralTemplate),
-    config.structSymbolEntryConfig.implementFromDisplayRule.emptyText,
-    config.structSymbolEntryConfig.implementFromDisplayRule.shouldShow && config.shouldShowImplementation,
-  );
+    // interface
+    const interfaceReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.interfaceSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.interfaceSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.interfaceSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.interfaceSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
 
-  // type alias
-  const typeAliasReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.typeAliasSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.typeAliasSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.typeAliasSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.typeAliasSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+    const interfaceImplementByCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.interfaceSymbolEntryConfig.implementByDisplayRule.singularTemplate),
+      new TemplateRenderer(config.interfaceSymbolEntryConfig.implementByDisplayRule.pluralTemplate),
+      config.interfaceSymbolEntryConfig.implementByDisplayRule.emptyText,
+      config.interfaceSymbolEntryConfig.implementByDisplayRule.shouldShow && config.shouldShowImplementation,
+    );
 
-  const typeAliasImplementFromCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.typeAliasSymbolEntryConfig.implementFromDisplayRule.singularTemplate),
-    new TemplateRenderer(config.typeAliasSymbolEntryConfig.implementFromDisplayRule.pluralTemplate),
-    config.typeAliasSymbolEntryConfig.implementFromDisplayRule.emptyText,
-    config.typeAliasSymbolEntryConfig.implementFromDisplayRule.shouldShow && config.shouldShowImplementation,
-  );
+    // child method of interface
+    const childMethodInterfaceReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.childMethodInterfaceSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
 
-  // constant
-  const constantReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.constantSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.constantSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.constantSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.constantSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+    const childMethodInterfaceImplementByCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.singularTemplate),
+      new TemplateRenderer(config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.pluralTemplate),
+      config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.emptyText,
+      config.childMethodInterfaceSymbolEntryConfig.implementByDisplayRule.shouldShow && config.shouldShowImplementation,
+    );
 
-  // var
-  const variableReferenceCodeLensMaker = new CodeLensMaker(
-    new TemplateRenderer(config.variableSymbolEntryConfig.referencesDisplayRule.singularTemplate),
-    new TemplateRenderer(config.variableSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
-    config.variableSymbolEntryConfig.referencesDisplayRule.emptyText,
-    config.variableSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
-  );
+    // method
+    const methodReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.methodSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.methodSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.methodSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.methodSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
 
-  const symbolHandlerRegistry = new SymbolHandlerRegistry([
-    new FunctionSymbolHandler(vsCodeWrapper, functionReferenceCodeLensMaker),
-    new InterfaceSymbolHandler(
-      vsCodeWrapper,
-      interfaceImplementByCodeLensMaker,
-      interfaceReferenceCodeLensMaker,
-      childMethodInterfaceImplementByCodeLensMaker,
-      childMethodInterfaceReferenceCodeLensMaker,
-    ),
-    new MethodSymbolHandler(vsCodeWrapper, methodImplementFromCodeLensMaker, methodReferenceCodeLensMaker),
-    new StructSymbolHandler(vsCodeWrapper, structImplementFromCodeLensMaker, structReferenceCodeLensMaker),
-    new TypeAliasSymbolHandler(vsCodeWrapper, typeAliasImplementFromCodeLensMaker, typeAliasReferenceCodeLensMaker),
-    new ConstantSymbolHandler(vsCodeWrapper, constantReferenceCodeLensMaker),
-    new VariableSymbolHandler(vsCodeWrapper, variableReferenceCodeLensMaker),
-  ]);
+    const methodImplementFromCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.methodSymbolEntryConfig.implementFromDisplayRule.singularTemplate),
+      new TemplateRenderer(config.methodSymbolEntryConfig.implementFromDisplayRule.pluralTemplate),
+      config.methodSymbolEntryConfig.implementFromDisplayRule.emptyText,
+      config.methodSymbolEntryConfig.implementFromDisplayRule.shouldShow && config.shouldShowImplementation,
+    );
 
-  const codeLensResultCacheByConfig: Record<CacheStrategyConfigKey, CodeLensResultCache> = {
-    NO_CACHE: new NoOpCodeLensCache(),
-    VERSION_AND_TIMESTAMP: new VersionAndTimestampCodeLensCache(),
-  };
+    // struct
+    const structReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.structSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.structSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.structSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.structSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
 
-  const codeLensProvider = new RelationCodeLensProvider(
-    vsCodeWrapper,
-    symbolHandlerRegistry,
-    codeLensResultCacheByConfig[config.cacheStrategy],
-  );
+    const structImplementFromCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.structSymbolEntryConfig.implementFromDisplayRule.singularTemplate),
+      new TemplateRenderer(config.structSymbolEntryConfig.implementFromDisplayRule.pluralTemplate),
+      config.structSymbolEntryConfig.implementFromDisplayRule.emptyText,
+      config.structSymbolEntryConfig.implementFromDisplayRule.shouldShow && config.shouldShowImplementation,
+    );
 
-  return codeLensProvider;
+    // type alias
+    const typeAliasReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.typeAliasSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.typeAliasSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.typeAliasSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.typeAliasSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
+
+    const typeAliasImplementFromCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.typeAliasSymbolEntryConfig.implementFromDisplayRule.singularTemplate),
+      new TemplateRenderer(config.typeAliasSymbolEntryConfig.implementFromDisplayRule.pluralTemplate),
+      config.typeAliasSymbolEntryConfig.implementFromDisplayRule.emptyText,
+      config.typeAliasSymbolEntryConfig.implementFromDisplayRule.shouldShow && config.shouldShowImplementation,
+    );
+
+    // constant
+    const constantReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.constantSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.constantSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.constantSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.constantSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
+
+    // var
+    const variableReferenceCodeLensMaker = new CodeLensMaker(
+      new TemplateRenderer(config.variableSymbolEntryConfig.referencesDisplayRule.singularTemplate),
+      new TemplateRenderer(config.variableSymbolEntryConfig.referencesDisplayRule.pluralTemplate),
+      config.variableSymbolEntryConfig.referencesDisplayRule.emptyText,
+      config.variableSymbolEntryConfig.referencesDisplayRule.shouldShow && config.shouldShowReference,
+    );
+
+    const symbolHandlerRegistry = new SymbolHandlerRegistry([
+      new FunctionSymbolHandler(this.vsCodeWrapper, functionReferenceCodeLensMaker),
+      new InterfaceSymbolHandler(
+        this.vsCodeWrapper,
+        interfaceImplementByCodeLensMaker,
+        interfaceReferenceCodeLensMaker,
+        childMethodInterfaceImplementByCodeLensMaker,
+        childMethodInterfaceReferenceCodeLensMaker,
+      ),
+      new MethodSymbolHandler(this.vsCodeWrapper, methodImplementFromCodeLensMaker, methodReferenceCodeLensMaker),
+      new StructSymbolHandler(this.vsCodeWrapper, structImplementFromCodeLensMaker, structReferenceCodeLensMaker),
+      new TypeAliasSymbolHandler(
+        this.vsCodeWrapper,
+        typeAliasImplementFromCodeLensMaker,
+        typeAliasReferenceCodeLensMaker,
+      ),
+      new ConstantSymbolHandler(this.vsCodeWrapper, constantReferenceCodeLensMaker),
+      new VariableSymbolHandler(this.vsCodeWrapper, variableReferenceCodeLensMaker),
+    ]);
+
+    const codeLensResultCacheByConfig: Record<CacheStrategyConfigKey, CodeLensResultCache> = {
+      NO_CACHE: new NoOpCodeLensCache(),
+      VERSION_AND_TIMESTAMP: new VersionAndTimestampCodeLensCache(),
+    };
+
+    return new RelationCodeLensProvider(
+      this.vsCodeWrapper,
+      symbolHandlerRegistry,
+      codeLensResultCacheByConfig[config.cacheStrategy],
+    );
+  }
 }
 
 export async function activate(context: vscode.ExtensionContext) {
   const vsCodeWrapper = new VsCodeWrapper();
   const configLoader = new VsCodeGoLensConfigurationLoader();
 
-  const codeLensProvider = createCodeLensProvider(configLoader, vsCodeWrapper);
-
-  let codeLensProviderDisposable = vscode.languages.registerCodeLensProvider(
-    { scheme: 'file', language: 'go' },
-    codeLensProvider,
-  );
+  const gogoCodeLensExtension = new GogoCodeLensExtension(vsCodeWrapper, configLoader);
+  gogoCodeLensExtension.register();
 
   const restartCommand = vscode.commands.registerCommand('gogoCodeLens.restart', () => {
-    configLoader.updateConfiguration();
-    codeLensProviderDisposable.dispose();
-
-    const newCodeLensProvider = createCodeLensProvider(configLoader, vsCodeWrapper);
-
-    codeLensProviderDisposable = vscode.languages.registerCodeLensProvider(
-      { scheme: 'file', language: 'go' },
-      newCodeLensProvider,
-    );
-
-    context.subscriptions.push(codeLensProviderDisposable);
+    gogoCodeLensExtension.register();
   });
 
   const configChangeListener = vscode.workspace.onDidChangeConfiguration((e) => {
-    if (!e.affectsConfiguration('gogoCodeLens')) {
-      return;
+    if (e.affectsConfiguration('gogoCodeLens')) {
+      gogoCodeLensExtension.register();
     }
-
-    configLoader.updateConfiguration();
-    codeLensProviderDisposable.dispose();
-
-    const newCodeLensProvider = createCodeLensProvider(configLoader, vsCodeWrapper);
-
-    codeLensProviderDisposable = vscode.languages.registerCodeLensProvider(
-      { scheme: 'file', language: 'go' },
-      newCodeLensProvider,
-    );
-
-    context.subscriptions.push(codeLensProviderDisposable);
   });
 
-  context.subscriptions.push(codeLensProviderDisposable, configChangeListener, restartCommand);
+  context.subscriptions.push(gogoCodeLensExtension, restartCommand, configChangeListener);
 }
 
 export function deactivate() {}
